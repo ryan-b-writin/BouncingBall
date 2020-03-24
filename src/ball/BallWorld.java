d9f700de05cc32f46e4e269a1ea6fd33e625e5f7
ryan-b-writin <ryan.bliton@gmail.com>
2020-03-23 23:00:15 -0400

    fixed multiball bug, only one nonfunctioning ball was drawn rather than the multiple functioning balls

--- src/ball/BallWorld.java
+++ src/ball/BallWorld.java
@@ -19,23 +19,16 @@ public class BallWorld extends JPanel {
 	private ControlPanel control;	
 	boolean paused;
 	
-	// Balls  
 	private static final int MAX_BALLS = 25; // Max number allowed  
 	private int currentNumBalls;             // Number currently active  
-	private Ball[] balls = new Ball[MAX_BALLS]; 
+	private Ball[] balls = new Ball[MAX_BALLS];
 	
-	/** Constructor to create the UI components and init the game objects. */  
+	/**  * Constructor to create the UI components and init the game objects.  
+	* Set the drawing canvas to fill the screen (given its width and height).
+	*  @param width : screen width  * @param height : screen height  */  
 	public BallWorld(int width, int height) {  
 		canvasWidth = width;  
 		canvasHeight = height;  
-		
-		// Init the ball at a random location (inside the box) and moveAngle  
-		Random rand = new Random(); 
-		int radius = 200;  
-		int x = rand.nextInt(canvasWidth - radius * 2 - 20) + radius + 10;  
-		int y = rand.nextInt(canvasHeight - radius * 2 - 20) + radius + 10; 
-		int speed = 5;  int angleInDegree = rand.nextInt(360);  
-		ball = new Ball(x, y, radius, speed, angleInDegree, Color.RED);  
 		
 		currentNumBalls = 11;  
 		balls[0] = new Ball(100, 410, 25, 3, 34, Color.YELLOW);  
@@ -49,9 +42,19 @@ public class BallWorld extends JPanel {
 		balls[8] = new Ball(250, 400, 50, 3, -42, Color.BLUE);  
 		balls[9] = new Ball(200, 80, 70, 6, -84, Color.CYAN);  
 		balls[10] = new Ball(500, 170, 90, 6, -42, Color.MAGENTA);  
+		
 		// The rest of the balls, that can be launched using the launch button  
 		for (int i = currentNumBalls; i < MAX_BALLS; ++i) {  
-			balls[i] = new Ball(20, canvasHeight - 20, 15, 5, 45, Color.RED);  }
+			balls[i] = new Ball(20, canvasHeight - 20, 15, 5, 45, Color.RED);  
+		}
+
+		// Init the ball at a random location (inside the box) and moveAngle  
+		Random rand = new Random(); 
+		int radius = 200;  
+		int x = rand.nextInt(canvasWidth - radius * 2 - 20) + radius + 10;  
+		int y = rand.nextInt(canvasHeight - radius * 2 - 20) + radius + 10; 
+		int speed = 5;  int angleInDegree = rand.nextInt(360);  
+		ball = new Ball(x, y, radius, speed, angleInDegree, Color.RED);  
 		
 		// Init the Container Box to fill the screen  
 		box = new ContainerBox(0, 0, canvasWidth, canvasHeight, Color.GRAY, Color.WHITE); 
@@ -114,44 +117,51 @@ public class BallWorld extends JPanel {
 		gameThread.start();  // Invoke GameThread.run()  
 	}  
 	
-	/** Update the game objects, detect collision and provide response. */  
-	public void gameUpdate() {  
-		float timeLeft = 1.0f;  // One time-step to begin with  
-		
-		// Repeat until the one time-step is up  
-		do {  
-			// Find the earliest collision up to timeLeft among all objects  
-			float tMin = timeLeft;  
-			
-			// Check collision between two balls  
-			for (int i = 0; i < currentNumBalls; ++i) {  
-				for (int j = 0; j < currentNumBalls; ++j) {  
-					if (i < j) {  
-						balls[i].intersect(balls[j], tMin);  
-						if (balls[i].earliestCollisionResponse.t < tMin) {  
-							tMin = balls[i].earliestCollisionResponse.t;  
-							}  
-						}  
-					}  
-				}  
-			
-			// Check collision between the balls and the box  
-			for (int i = 0; i < currentNumBalls; ++i) {  
-				balls[i].intersect(box, tMin);  
-				if (balls[i].earliestCollisionResponse.t < tMin) {  
-					tMin = balls[i].earliestCollisionResponse.t;  
-					}  
-				}  
-			
-			// Update all the balls up to the detected earliest collision time tMin,  
-			// or timeLeft if there is no collision.  
-			for (int i = 0; i < currentNumBalls; ++i) {  
-				balls[i].update(tMin);  
-				} 
-			timeLeft -= tMin;                // Subtract the time consumed and repeat  
-		} while (timeLeft > EPSILON_TIME);  // Ignore remaining time less than threshold  
-	}
+	/**  
+	 * One game time-step.  
+	 * Update the game objects, with proper collision detection and response.  
+	 */  
+public void gameUpdate() {
+        
+        int currentNumBalls = balls.length;
+          
+        float timeLeft = 1.0f;  // One time-step to begin with
 
+        // Repeat until the one time-step is up 
+        do {
+           // Find the earliest collision up to timeLeft among all objects
+           float tMin = timeLeft;
+
+           // Check collision between two balls
+           for (int i = 0; i < currentNumBalls; i++) {
+              for (int j = 0; j < currentNumBalls; j++) {
+                 if (i < j) {
+                    balls[i].intersect(balls[j], tMin);
+                    if (balls[i].earliestCollisionResponse.t < tMin) {
+                       tMin = balls[i].earliestCollisionResponse.t;
+                    }
+                 }
+              }
+           }
+           // Check collision between the balls and the box
+           for (int i = 0; i < currentNumBalls; i++) {
+              balls[i].intersect(box, tMin);
+              if (balls[i].earliestCollisionResponse.t < tMin) {
+                 tMin = balls[i].earliestCollisionResponse.t;
+              }
+           }
+
+           // Update all the balls up to the detected earliest collision time tMin,
+           // or timeLeft if there is no collision.
+           for (int i = 0; i < currentNumBalls; i++) {
+              balls[i].update(tMin);
+           }
+
+           timeLeft -= tMin;                // Subtract the time consumed and repeat
+        } while (timeLeft > EPSILON_TIME);  // Ignore remaining time less than threshold
+     
+	}
+	
 	/** The custom drawing panel for the bouncing ball (inner class). */  
 	class DrawCanvas extends JPanel {  
 		/** Custom drawing codes */  
@@ -160,8 +170,10 @@ public class BallWorld extends JPanel {
 			super.paintComponent(g);    // Paint background  
 			
 			// Draw the box and the ball  
-			box.draw(g);  
-			ball.draw(g);  
+			box.draw(g); 
+			for(int i=0 ; i < balls.length ; i++) {
+				balls[i].draw(g); 
+			}
 			
 			// Display ball's information  
 			g.setColor(Color.WHITE);  
@@ -181,10 +193,9 @@ public class BallWorld extends JPanel {
 	
 	/** The control panel (inner class). */  
 	class ControlPanel extends JPanel {  
-		
-		/** Constructor to initialize UI components */  
+		/** Constructor to initialize UI components of the controls */  
 		public ControlPanel() {  
-			// A checkbox to toggle pause/resume all the balls' movement  
+			// A checkbox to toggle pause/resume movement  
 			JCheckBox pauseControl = new JCheckBox();  
 			this.add(new JLabel("Pause"));  
 			this.add(pauseControl);  
@@ -194,62 +205,51 @@ public class BallWorld extends JPanel {
 					paused = !paused;  // Toggle pause/resume flag  
 				}  
 			});  
-			
-			// A slider for adjusting the speed of all the balls by a factor  
-			final float[] ballSavedSpeedXs = new float[MAX_BALLS];  
-			final float[] ballSavedSpeedYs = new float[MAX_BALLS];  
-			for (int i = 0; i < currentNumBalls; ++i) {  
-				ballSavedSpeedXs[i] = balls[i].speedX;  
-				ballSavedSpeedYs[i] = balls[i].speedY;  
-			}  
-			int minFactor = 5;    // percent  
-			int maxFactor = 200;  // percent  
-			
-			JSlider speedControl = new JSlider(JSlider.HORIZONTAL, minFactor, maxFactor, 100);  
+			// A slider for adjusting the speed of the ball  
+			int minSpeed = 2;  
+			int maxSpeed = 20;  
+			JSlider speedControl = new JSlider(JSlider.HORIZONTAL, minSpeed, maxSpeed,(int)ball.getSpeed());  
 			this.add(new JLabel("Speed"));  
 			this.add(speedControl);  
-			speedControl.addChangeListener(new ChangeListener() {  
+			speedControl.addChangeListener((ChangeListener) new ChangeListener() {  
 				@Override  
 				public void stateChanged(ChangeEvent e) {  
 					JSlider source = (JSlider)e.getSource();  
 					if (!source.getValueIsAdjusting()) {  
-						int percentage = (int)source.getValue();  
-						for (int i = 0; i < currentNumBalls; ++i) {  
-							balls[i].speedX = ballSavedSpeedXs[i] * percentage / 100.0f;  
-							balls[i].speedY = ballSavedSpeedYs[i] * percentage / 100.0f;  
-							}  
-						}  
+						int newSpeed = (int)source.getValue();  
+						int currentSpeed = (int)ball.getSpeed();  
+						ball.speedX *= (float)newSpeed / currentSpeed ;  
+						ball.speedY *= (float)newSpeed / currentSpeed;  
 					}  
-				});  
-			
-			// A button for launching the remaining balls  
-			final JButton launchControl = new JButton("Launch New Ball");  
-			this.add(launchControl);  
-			launchControl.addActionListener(new ActionListener() {  
+				}  
+			});  
+			// A slider for adjusting the radius of the ball  
+			int minRadius = 10;  
+			int maxRadius = ((canvasHeight > canvasWidth) ? canvasWidth: canvasHeight) / 2 - 8;  
+			JSlider radiusControl = new JSlider(JSlider.HORIZONTAL, minRadius,maxRadius,(int)ball.radius);  
+			this.add(new JLabel("Ball Radius"));  
+			this.add(radiusControl);  
+			radiusControl.addChangeListener(new ChangeListener() {  
 				@Override  
-				public void actionPerformed(ActionEvent e) {  
-					if (currentNumBalls < MAX_BALLS) {  
-						++currentNumBalls;  
-						if (currentNumBalls == MAX_BALLS) {  
-							// Disable the button, as there is no more ball  
-							launchControl.setEnabled(false);  
-							}  
+				public void stateChanged(ChangeEvent e) {  
+					JSlider source = (JSlider)e.getSource();  
+					if (!source.getValueIsAdjusting()) {  
+						float newRadius = source.getValue();  
+						ball.radius = newRadius;  
+						// Reposition the ball such as it is inside the box  
+						if (ball.x - ball.radius < box.minX) {  
+							ball.x = ball.radius + 1;  
+						} else if (ball.x + ball.radius > box.maxX) {  
+							ball.x = box.maxX - ball.radius - 1;  
+						}  
+						if (ball.y - ball.radius < box.minY) {  
+							ball.y = ball.radius + 1;  
+						} else if (ball.y + ball.radius > box.maxY) {  
+							ball.y = box.maxY - ball.radius - 1;  
 						}  
 					}  
-				});  
-			}  
+				}  
+			});  
 		}  
-	}
-						
-					
-				
-			
-						
-					
-				
-			
-			
-				
-			
-		
-	
+	}  
+}
